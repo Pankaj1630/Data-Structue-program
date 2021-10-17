b26ab21024f89dec86c82b0b72623a723115fd8e
Pankaj <pkv1630@gmail.com>
2021-10-17 20:13:16 +0530

    Some modification is done in bubblesort.c and added a new file quick_sort.c

---
+++ quick_sort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int arr[100], i;
+    printf("Enter size of the array you want to sort:\n");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the element of the array:\n");
+        scanf("%d", &arr[i]);
+        /* code */
+    }
+
+    return 0;
+}
