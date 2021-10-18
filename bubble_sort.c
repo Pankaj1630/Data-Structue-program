1b7e39e9a6d16f7923386c484cb5334899089155
Pankaj <pkv1630@gmail.com>
2021-10-18 23:23:03 +0530

    Program for bubble sort

---
+++ bubble_sort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+void print(int a[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+void bubble(int a[], int n)
+{
+    int temp, i, j;
+    int issort = 0;
+    for (i = 0; i < n; i++)
+    {
+        printf("\nThe number of passes used to sort the array is %d\n ", i + 1);
+        issort = 1;
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[i])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                issort = 0;
+            }
+        }
+        if (issort)
+        {
+            return;
+        }
+    }
+}
+
+int main()
+{
+    int i,n, a[100];
+    printf("Enter the size of array:\n");
+    scanf("%d", &n);
+    printf("Enter the element of the array:\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("The value before sorting the array\n");
+    print(a, n);
+    printf("The value after sorting the array\n");
+    bubble(a, n);
+    print(a, n);
+    return 0;
+}
